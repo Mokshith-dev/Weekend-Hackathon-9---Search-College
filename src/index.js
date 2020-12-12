c0f7055e195ac80076aff0fb7857a379127a1fc1
Mokshith-dev <mokshith.ajalade@gmail.com>
2020-12-12 18:13:44 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,8 +7,21 @@ const port = 8080
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connections } = require('mongoose');
+const { connection } = require('./connector')
+app.get('/findColleges', async (req,res) => {
+    const {name, city, state, exam, course, maxFees, minPackage } = req.body
+    if(maxFees < 0 || minPackage < 0) {
+        res.status(400);
+        return;
+    }
+    
+    // const data = await connection.find({"name":{"$regex": name, "$options": "i" }, state: state, city: city, minPackage: {$gte: Number(minPackage)}, maxFees: {$lte: Number(maxFees)}, course: course, exam: [exam]} );
+    const data = await connection.find({"name":{"$regex": name, "$options": "i" },state: state, city: city, minPackage: {$gte: Number(minPackage)}, maxFees: {$lte: Number(maxFees)}, course: course, exam: exam} );
 
+    console.log(data);
+    res.send(data);
 
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
